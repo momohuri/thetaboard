6b61beb992e90438ecf35ac3296556320cd0e71a
Adrien Vinches <adrien@sightcall.com>
2021-03-26 13:57:24 -0700

    clean unused import

--- thetaboard-ui/app/components/guardian-status.js
+++ thetaboard-ui/app/components/guardian-status.js
@@ -1,8 +1,5 @@
-import Ember from 'ember';
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import $ from 'jquery';
 import { cancel } from '@ember/runloop';
 
 export default class GuardianStatusComponent extends Component {
