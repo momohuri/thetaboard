ab174c6dd34eb11d56c1c158c484165328c4c303
Adrien Vinches <adrien@sightcall.com>
2021-03-29 20:31:56 -0700

    stake deposit only when guardian is up

--- thetaboard-ui/app/components/theta-stakes/theta-stakes-deposit.js
+++ thetaboard-ui/app/components/theta-stakes/theta-stakes-deposit.js
@@ -3,4 +3,5 @@ import { inject as service } from '@ember/service';
 
 export default class ThetaStakesThetaStakesDepositComponent extends Component {
   @service('theta-stakes') thetaStakes;
+  @service('guardian') guardian;
 }
