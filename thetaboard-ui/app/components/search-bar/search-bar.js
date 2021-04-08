8186ffcae357af21c79efe6a76c281c77869e73b
Adrien Vinches <adrien@sightcall.com>
2021-04-07 18:45:45 -0700

    search bar

---
+++ thetaboard-ui/app/components/search-bar/search-bar.js
@@ -0,0 +1,23 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class SearchBarSearchBarComponent extends Component {
+  @service('theta-sdk') thetaSdk;
+  @service('utils') utils;
+  walletAddress = '';
+
+  @action 
+  async search(event) {
+    event.preventDefault();
+    if (
+      this.walletAddress.length == 42 &&
+      this.walletAddress.substr(1, 1).toLocaleLowerCase() == 'x'
+    ) {
+      await this.thetaSdk.getWalletInfo([this.walletAddress]);
+    } else {
+      this.utils.errorNotify("Invalid Wallet Address");
+    }
+    $('#searchModal').modal('hide');
+  }
+}
